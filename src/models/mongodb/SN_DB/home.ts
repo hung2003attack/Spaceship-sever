16442fc67c8ef516b8c45093b721f99f2d8cff31
hungaa1 <nguyentronghung05072003@gmail.com>
2023-07-03 18:58:29 +0700

    h

--- src/models/mongodb/SN_DB/home.ts
+++ src/models/mongodb/SN_DB/home.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 const feel = {
-    like: { type: Number(11), default: 0 },
-    love: { type: Number(11), default: 0 },
-    smile: { type: Number(11), default: 0 },
-    sad: { type: Number(11), default: 0 },
+    like: { act: { type: Number(11), defaultValue: 0 }, id_user: [String(50)] },
+    love: { act: { type: Number(11), defaultValue: 0 }, id_user: [String(50)] },
+    smile: { act: { type: Number(11), defaultValue: 0 }, id_user: [String(50)] },
+    sad: { act: { type: Number(11), defaultValue: 0 }, id_user: [String(50)] },
 };
 const comments = [
     {
         id_user: { type: String(50), required: true },
         content: {
             text: { type: String, text: String },
-            imageOrVideos: [{ file: { type: String(50) }, title: { type: String(30) }, feel }],
+            imageOrVideos: [{ file: { type: String(50) }, feel }],
         },
         feel,
         reply: [
@@ -19,8 +19,10 @@ const comments = [
                 id_user: { type: String(50), required: true },
                 content: { text: { type: String, text: String }, imageOrVideos: [String(50)] },
                 feel,
+                private: { type: Boolean, defaultValue: false },
             },
         ],
+        private: { type: Boolean, defaultValue: false },
     },
 ];
 const Posts = new Schema({
@@ -28,13 +30,21 @@ const Posts = new Schema({
     type: { type: Number(1) },
     content: {
         text: { type: String, text: String },
-        imageOrVideos: [{ file: { type: String(50) }, love: { type: String(11), defaultValue: 0 }, comments }],
+        imageOrVideos: [
+            {
+                file: { type: String(50) },
+                love: { act: { type: Number(11), defaultValue: 0 }, id_user: [String(50)] },
+                comments,
+                options: { title: { type: String(100) }, Bgcolors: { type: String(10) }, column: { type: Number(2) } },
+            },
+        ],
     },
     feel,
     amountComments: { type: Number(11), default: 0 },
     commentsOne: comments,
     commentsTwo: comments,
     commentsThree: comments,
+    private: { type: Boolean, defaultValue: false },
     createdAt: { type: Date, required: true, default: Date.now() },
 });
 export const NewPost = mongoose.model('NewPost', Posts);
